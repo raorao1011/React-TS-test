2bb48919179fe8e08bbd2a7c6dff8be4ae9c63a4
rao <68511759+raorao1011@users.noreply.github.com>
2021-04-23 18:15:58 +0900

    Initial commit

---
+++ src/components/pages/Users.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import styled from "styled-components";
+import { useLocation } from "react-router-dom";
+
+import { SearchInput } from "../molecules/SearchInput";
+import { UserCard } from "../organisms/user/UserCard";
+const users = [...Array(10).keys()].map((val) => {
+  return {
+    id: val,
+    name: `Yuki-${val}`,
+    image: "https://source.unsplash.com/NE0XGVKTmcA",
+    email: "12345@example.com",
+    phone: "090-1111-1111",
+    company: {
+      name: "テスト株式会社"
+    },
+    website: "https://google.com"
+  };
+});
+
+export const Users = () => {
+  const { state } = useLocation();
+  const isAdmin = () => (state ? state.isAdmin : false); //stateが無い場合の対処
+  //trueならstate.isAdmin, nullとfalseならfalseを返す
+
+  return (
+    <SContainer>
+      <h2>ユーザー一覧</h2>
+      <SearchInput />
+      <SUserArea>
+        {users.map((obj) => (
+          <UserCard key={obj.id} user={obj} isAdmin={isAdmin} />
+        ))}
+      </SUserArea>
+    </SContainer>
+  );
+};
+
+const SContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 24px;
+`;
+
+const SUserArea = styled.div`
+  padding-top: 40px;
+  width: 100%;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+  grid-gap: 20px;
+`;
